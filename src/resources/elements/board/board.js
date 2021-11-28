10e2e274cefc7cc1a4b625d5464e33170a0be4e7
Esger <esgerj@gmail.com>
2021-11-28 03:40:41 +0100

    Vullen naast elkaar

--- src/resources/elements/board/board.js
+++ src/resources/elements/board/board.js
@@ -5,8 +5,10 @@ import { EventAggregator } from 'aurelia-event-aggregator';
 export class BoardCustomElement {
 
     bricks = [];
-    bricksCount = 10;
+    blocks = [];
+    bricksCount = 25;
     blockSize = 5;
+    boardSize = 100 / this.blockSize;
 
     constructor(eventAggregator, element) {
         this._element = element;
@@ -25,17 +27,54 @@ export class BoardCustomElement {
         return Math.floor(Math.random() * max);
     }
 
+    _getBlockPosition(position, direction) {
+        const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+        const position2 = {
+            left: position.left + directions[direction][0],
+            top: position.top + directions[direction][1]
+        };
+        return position2;
+    }
+
+    _positionIsFree(position, direction) {
+        const position2 = this._getBlockPosition(position, direction);
+        const isFree = !this.blocks.some(block => block.left == position.left && block.top == position.top);
+        const p2isFree = !this.blocks.some(block => block.left == position2.left && block.top == position2.top);
+        const withinBounds =
+            position.left >= 0 && position.left < this.boardSize - 1 &&
+            position.top >= 0 && position.top < this.boardSize - 1 &&
+            position2.left >= 0 && position2.top < this.boardSize - 1 &&
+            position2.top >= 0 && position2.top < this.boardSize - 1;
+        // !withinBounds && console.log(position, direction);
+        return isFree && p2isFree && withinBounds;
+    }
+
+    _setPosition(brick) {
+        let positionFound;
+        const position = {}
+        let direction;
+        do {
+            direction = this._randomNumberWithin(4);
+            position.left = this._randomNumberWithin(this.boardSize);
+            position.top = this._randomNumberWithin(this.boardSize);
+            positionFound = this._positionIsFree(position, direction);
+        } while (!positionFound);
+        console.log(position, direction);
+        brick.position = position;
+        brick.direction = direction;
+    }
+
     _scatterBricks() {
         for (let i = 0; i < this.bricksCount; i++) {
             const brick = {
                 index: i,
-                direction: this._randomNumberWithin(4),
-                position: {
-                    left: this._randomNumberWithin(100 / this.blockSize) * this.blockSize,
-                    top: this._randomNumberWithin(100 / this.blockSize) * this.blockSize,
-                }
+                position: {},
+                direction: undefined
             }
+            this._setPosition(brick);
             this.bricks.push(brick);
+            this.blocks.push(brick.position);
+            this.blocks.push(this._getBlockPosition(brick.position, brick.direction));
         }
     }
 
